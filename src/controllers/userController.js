6df4b19c3d07ae327d37e0416b3474e7c39661c4
shabnum1 <shabnumhuda7259@gmail.com>
2022-07-25 05:59:28 -0230

    validation

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,16 +1,61 @@
 
 const userModel = require('../models/userModel')
+const { keyValue,isValid,isValidEmail,passwordRegex ,phoneRegex} = require("../validators/validator");
 
 
 const createUser = async (req, res) => {
     try {
-        let finalDetails = req.body
+        let details = req.body
         let files = req.files
-       // let { fname, lname, email, password, phone } = data
-        
-        
-       // const finalDetails = { fname, lname, email, profileImage, password, phone, address }
-        let savedData = await userModel.create(finalDetails)
+       let { fname, lname, email, password, phone, address } = details
+
+       if (!keyValue(details)) {
+        return res.status(400).send({ status: false, message: "please provide user data" })
+    }
+
+     if (!isValid(fname)) {
+        return res.status(400).send({ status: false, messege: "please provide name" })
+    }
+    if (!isValid(lname)) {
+        return res.status(400).send({ status: false, messege: "please provide lname" })
+    }
+
+    if (!isValid(email)) {
+        return res.status(400).send({ status: false, messege: "please provide email" })
+    }
+
+    if (!isValidEmail(email)) {
+        return res.status(400).send({ status: false, message: "Please provide valid Email Address" });
+    }
+
+    let isDuplicateEmail = await userModel.findOne({ email })
+    if (isDuplicateEmail) {
+        return res.status(400).send({ status: false, message: "email already exists" })
+    }
+
+    if (!isValid(phone)) {
+        return res.status(400).send({ status: false, messege: "please provide phone number" })
+    }
+    if (!phoneRegex(phone)) {
+    return res.status(400).send({ status: false, msg: "phone number is invalid!" })  // 7th V used here
+    }
+    let duplicatePhone = await userModel.findOne({ phone })        // DB Call
+
+    if (duplicatePhone) return res.status(400).send({ status: false, msg: "phone number is already registered!" }) 
+    
+    if (!isValid(password)) {
+        return res.status(400).send({ status: false, messege: "please provide password" })
+    }
+
+    if (!passwordRegex(password)) {
+        return res.status(400).send({ status: false, messege: "invalid password" })
+    }
+ 
+    if(!address){
+            return res.status(400).send({status:false, message:"please provide address"})
+        }
+       
+        let savedData = await userModel.create(details)
         return res.status(201).send({ status: true, msg: "user created successfully", data: savedData });
     }
     catch (error) {
