67948f3d3b8cec97b02549b79c8d72612229fcec
Sahil Singh Baghel <102261017+sahil3487@users.noreply.github.com>
2022-08-07 23:05:02 +0530

    final

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,33 +1,33 @@
 const jwt = require("jsonwebtoken");
 
-const mongoose=require("mongoose")
+const mongoose = require("mongoose")
 const userModel = require("../models/userModel");
 
 //--------------------Authneication-------------------
 
-const AuthenticationCheck= async function(req,res,next){
-  try{
-    let token= req.headers.authorization;
+const AuthenticationCheck = async function (req, res, next) {
+  try {
+    let token = req.headers.authorization;
     if (!token) return res.send({ status: false, message: "token must be present" });
-   let bearerToken = token.split(" ")[1]
-  
-   
-   jwt.verify(bearerToken, "Project5-Group3",function (err, decodedToken) {
+    let bearerToken = token.split(" ")[1]
+
+
+    jwt.verify(bearerToken, "Project5-Group3", function (err, decodedToken) {
       if (err) {
-           return res.status(401).send({ status: false, message: "invalid token" })
+        return res.status(401).send({ status: false, message: "invalid token" })
       } else {
-          console.log(decodedToken)
-          req.loggedInUserId = decodedToken.userId
-          next()
+        console.log(decodedToken)
+        req.loggedInUserId = decodedToken.userId
+        next()
       }
-  })
-  
-}
-catch(err){
-  return res.status(500).send({status:false,message:err.message})
-}
+    })
+
+  }
+  catch (err) {
+    return res.status(500).send({ status: false, message: err.message })
+  }
 }
 
 
 
-module.exports.AuthenticationCheck=AuthenticationCheck
+module.exports.AuthenticationCheck = AuthenticationCheck
